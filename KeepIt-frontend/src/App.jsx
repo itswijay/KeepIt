1a5339da058b9d64f0e84cd18821346c2ec6e0a6
Pubudu Wijesundara <neo17641@gmail.com>
2025-09-29 14:13:33 +0530

    config tailwind and shadcn

--- KeepIt-frontend/src/App.jsx
+++ KeepIt-frontend/src/App.jsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
-    <h1>KeepIt</h1>
+    <h1 className="text-amber-500">KeepIt</h1>
+    <Button className="bg-amber-200 text-black mt-4 ml-4">Hii</Button>
     </>
   )
 }
