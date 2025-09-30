10def63e30c44fea4a91ceded89846abca41b05d
Pubudu Wijesundara <neo17641@gmail.com>
2025-09-30 22:16:09 +0530

    Initialize the django backend, create .env, .gitignore file and remove unnecesary things in the frontend

--- KeepIt-frontend/src/App.jsx
+++ KeepIt-frontend/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <>
     <h1 className="text-amber-500">KeepIt</h1>
-    <Button className="bg-amber-200 text-black mt-4 ml-4">Hii</Button>
+    <div className="flex flex-col items-center justify-center h-screen">
+    <Button className="bg-amber-200">Hello</Button>
+    </div>
     </>
   )
 }
