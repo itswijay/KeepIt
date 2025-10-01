aa485a74b2f290b980cb96d671ba4f975bcfadb5
Pubudu Wijesundara <neo17641@gmail.com>
2025-10-01 10:33:09 +0530

    Add responsiveness for the home page

--- KeepIt-frontend/src/components/Header.jsx
+++ KeepIt-frontend/src/components/Header.jsx
@@ -13,15 +13,19 @@ import { Button } from './ui/button'
 
 const Header = () => {
   return (
-    <div className='w-full flex justify-center py-4'>
-      <NavigationMenu className='w-screen max-w-none flex justify-around'>
-        <NavigationMenuList className=''>
+    <div className="w-full flex justify-center py-4">
+      <NavigationMenu className="w-screen max-w-none flex justify-around">
+        <NavigationMenuList className="">
           <NavigationMenuItem>KeepIt</NavigationMenuItem>
         </NavigationMenuList>
         <NavigationMenuList>
           <NavigationMenuItem>
-            <Button variant='outline'>Login</Button>
-            <Button className='ml-4'>Register</Button>
+            <Button variant="outline" className="text-xs md:text-[14px]">
+              Login
+            </Button>
+            <Button className="ml-2 md:ml-4 text-xs md:text-[14px]">
+              Register
+            </Button>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
--- KeepIt-frontend/src/components/Main.jsx
+++ KeepIt-frontend/src/components/Main.jsx
@@ -4,7 +4,7 @@ import { Button } from './ui/button'
 const Main = () => {
   return (
     <div className="text-center w-full flex-1 flex items-center justify-center">
-      <div className="bg-[#444141]/60 px-24 py-24 rounded-2xl">
+      <div className="bg-[#444141]/60 p-8 md:p-24 rounded-2xl">
         <h1 className="text-xl md:text-3xl lg:text-4xl font-bold">
           Keep Your Notes safe
         </h1>
