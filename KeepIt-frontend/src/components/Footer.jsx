12d0a7ca01227004912d1d766b648ebaccb4ec64
Pubudu Wijesundara <neo17641@gmail.com>
2025-10-24 23:01:55 +0530

    update footer background color

--- KeepIt-frontend/src/components/Footer.jsx
+++ KeepIt-frontend/src/components/Footer.jsx
@@ -4,9 +4,9 @@ const year = new Date().getFullYear()
 const Footer = () => {
   return (
     <>
-      <footer className="fixed bottom-0 left-0 right-0 z-20 flex flex-col justify-center items-center py-2">
-        <hr className="w-full border border-bottom border-[#444141]" />
-        <p className="text-center text-xs pt-2">
+      {/* <hr className="w-full border border-bottom border-[#444141]" /> */}
+      <footer className="fixed bottom-0 left-0 right-0 z-20 flex flex-col justify-center items-center py-2 bg-[#444141]">
+        <p className="text-center text-xs pt-1">
           &copy; {year} Built with passion - Pubudu Wijesundara
         </p>
       </footer>
