d6003504106ca67b2e4960dee191d38c2024384d
Pubudu Wijesundara <neo17641@gmail.com>
2025-10-24 22:58:46 +0530

    feat: create note component in dashbord with two props which are title and note

--- KeepIt-frontend/src/pages/Dashboard.jsx
+++ KeepIt-frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,54 @@
 import React, { useEffect } from 'react'
 import axioInstance from '@/axiosInstance'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const Note = (props) => {
+  return (
+    <Card className="w-full max-w-sm h-[200px]">
+      <CardHeader>
+        <CardTitle>{props.title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>{props.note}</p>
+      </CardContent>
+    </Card>
+  )
+}
 
 const Dashboard = () => {
+  useEffect(() => {
+    const fetchProtectedData = async () => {
+      try {
+        const response = await axioInstance.get('/protected-view')
+        console.log('Success: ', response.data)
+      } catch (error) {
+        console.error('Error fetching data', error)
+      }
+    }
+    fetchProtectedData()
+  }, [])
 
-    useEffect(() => {
-        const fetchProtectedData = async () => {
-            try {
-                const response = await axioInstance.get(
-                  '/protected-view'
-                )
-                console.log("Success: ", response.data)
-            } catch (error) {
-                console.error("Error fetching data",error)
-            }
-        }
-        fetchProtectedData()
-    },[])
-
-  return <div>Dashboard</div>
+  return (
+    <>
+      <div className="w-full min-h-screen pt-12 px-16 grid grid-cols-4 gap-6">
+<Note title="Hello world" note="bla bla bla"/>
+<Note />
+<Note />
+<Note />
+      </div>
+    </>
+  )
 }
 
 export default Dashboard
